5d1c32de7565859ca26bc4eb5aedf4bc6a942ea0
zviel <zvielkoren@gmail.com>
2025-03-26 21:29:19 +0200

    refactor redirect logic; update profile navigation based on user authentication

--- app/index.tsx
+++ app/index.tsx
@@ -4,5 +4,5 @@ import { useAuth } from "@/contexts/AuthContext";
 export default function Index() {
   const { user } = useAuth();
 
-  return <Redirect href={user ? "/" : "/(auth)/login"} />;
+  return <Redirect href={user ? "/profile" : "/(auth)/login"} />;
 }
